cdb524eb71a65a4cf9403fa6578003da0652fbc5
rnrl1215 <d1215@naver.com>
2021-07-18 16:14:44 +0900

    modified item controller

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -34,5 +38,29 @@ public class ItemController {
         itemService.save(book);
         return "redirect:/";
     }
-}
 
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+    /**
+     * 상품 수정 폼
+     */
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+}
